db6fe218a6a6757021143b34492f6d103d2e22ba
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-02-26 13:33:24 +0100

    Initial commit

--- app-variables.tf
+++ app-variables.tf
@@ -2,27 +2,31 @@
 ## Application - Variables ##
 #############################
 
+#############################
+## Application - Variables ##
+#############################
+
 # company name 
 variable "company" {
   type        = string
-  description = "This variable defines thecompany name used to build resources"
+  description = "The company name used to build resources"
 }
 
 # application name 
 variable "app_name" {
   type        = string
-  description = "This variable defines the application name used to build resources"
+  description = "The application name used to build resources"
 }
 
-# application or company environment
+# environment
 variable "environment" {
   type        = string
-  description = "This variable defines the environment to be built"
+  description = "The environment to be built"
 }
 
 # azure region
 variable "location" {
   type        = string
-  description = "Azure region where the resource group will be created"
+  description = "Azure region where the resources will be created"
   default     = "north europe"
 }
